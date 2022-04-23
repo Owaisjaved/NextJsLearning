686bb6e66af3b0b4524f12eebb12e64f32e02b4c
OwaisJaved <owaisjaved954@gmail.com>
2022-04-23 13:34:42 +0200

    about page

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About Page</h1>
+            <p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem </p>
+            <p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem </p>
+            <p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem </p>
+        </div>
+     );
+}
+ 
+export default About;
