7ebce2ddcb9706a1c651f057dc812f4a564e420b
OwaisJaved <owaisjaved954@gmail.com>
2022-04-24 12:27:26 +0200

    Added Head Tag

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,18 @@
+import Head from "next/head";
 const About = () => {
-    return ( 
+    return (
+        <>
+        <Head>
+    <title>Ninja List | About</title>
+    <meta name="keywords" content="ninjas" />
+    </Head>
         <div>
             <h1>About Page</h1>
             <p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem </p>
             <p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem </p>
             <p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem </p>
         </div>
+        </>
      );
 }
  
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
+    <>
+    <Head>
+    <title>Ninja List | Home</title>
+    <meta name="keywords" content="ninjas" />
+    </Head>
     <div>
       <h1 className={styles.title}> Hello World</h1>
       <p className={styles.text}>
@@ -21,5 +27,6 @@ export default function Home() {
         <a className={styles.btn}>Ninja Listing</a>
       </Link>
     </div>
+    </>
   );
 }
